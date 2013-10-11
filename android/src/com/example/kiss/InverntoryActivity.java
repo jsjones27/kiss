8ba7b8aeca3430ad75cdf9f7d4d9fee0384cb532
Jonah <jjmgruber@gmail.com>
2013-10-11 13:11:21 -0400

    added activities for inventory and grocery

--- android/src/com/example/kiss/InverntoryActivity.java
+++ android/src/com/example/kiss/InverntoryActivity.java
@@ -4,23 +4,19 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class InverntoryActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_inverntory);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.inverntory, menu);
 		return true;
-	}
-	
-	public void jumpToInventory(){
-		return;
 	}
 
 }
