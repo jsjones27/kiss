464d1db4a25d14d287796cc754effa8f49bc4bd2
Jonah <jjmgruber@gmail.com>
2013-10-11 14:11:56 -0400

    MainActivity now has working buttons!

--- android/src/com/example/kiss/InverntoryActivity.java
+++ android/src/com/example/kiss/InverntoryActivity.java
@@ -2,21 +2,23 @@ package com.example.kiss;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 
 public class InverntoryActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		Intent intent = getIntent();
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_inverntory);
 	}
-
+/*
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.inverntory, menu);
 		return true;
 	}
-
+*/
 }
