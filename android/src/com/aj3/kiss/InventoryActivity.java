cb218c33a4877aaa69fc64dd621a6f3b8b504d3b
Jonah <jjmgruber@gmail.com>
2013-10-28 16:52:45 -0400

    Create a common class ItemListActivity which InventoryActivity and GroceryAvtivity now extend

--- android/src/com/aj3/kiss/InventoryActivity.java
+++ android/src/com/aj3/kiss/InventoryActivity.java
@@ -1,85 +1,26 @@
 package com.aj3.kiss;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.content.Intent;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.Toast;
 import com.aj3.kiss.R;
-
 import java.util.List;
-import java.util.ArrayList;
 
-public class InventoryActivity extends Activity {
+public class InventoryActivity extends ItemListActivity {
 	public static final String NAME = "inventory";
 
-	ListView listView;
+//	ListView listView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_inventory);
-		
-		// Get ListView object from xml
-		listView = (ListView) findViewById(R.id.list);
 
 		DatabaseHelper db = new DatabaseHelper(this);
 		List<ListItem> listItems = db.getInventory();
-		List<String> values = new ArrayList<String>();
-		for (ListItem li : listItems) {
-			values.add(li.getItem().getName());
-		}
 		db.close();
 		
-		
-		// Define a new Adapter
-		// First parameter - Context
-		// Second parameter - Layout for the row
-		// Third parameter - ID of the TextView to which the data is written
-		// Forth - the Array of data
+		this.displayList(listItems);
 
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-		  android.R.layout.simple_list_item_1, android.R.id.text1, values);
-	   
-
-		// Assign adapter to ListView
-		listView.setAdapter(adapter); 
-		/* 
-		// ListView Item Click Listener
-		listView.setOnClickListener(new OnClickListener() {
-
-			  public void onItemClick(AdapterView<?> parent, View view,
-				 int position, long id) {
-				
-			   // ListView Clicked item index
-			   int itemPosition	 = position;
-			   
-			   // ListView Clicked item value
-			   String  itemValue	= (String) listView.getItemAtPosition(position);
-				  
-				// Show Alert 
-				Toast.makeText(getApplicationContext(),
-				  "Position :"+itemPosition+"  ListItem : " +itemValue , Toast.LENGTH_LONG)
-				  .show();
-			 
-			  }
-
-			@Override
-			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-
-		 }); 
-		 */
 	}
 
 	@Override
@@ -87,22 +28,5 @@ public class InventoryActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.inventory, menu);
 		return true;
-	}
-	
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle presses on the action bar items
-		switch (item.getItemId()) {
-			case R.id.action_add_item:
-				Intent intent = new Intent(this, AddItemActivity.class);
-				intent.putExtra(AddItemActivity.ACTIVITY_CALLER, this.NAME);
-				startActivity(intent);
-				return true;
-			case R.id.action_settings:
-				
-				return true;
-			default:
-				return super.onOptionsItemSelected(item);
-		}
 	}
 }
