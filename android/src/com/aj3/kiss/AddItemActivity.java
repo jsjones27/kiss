b8c47a43f142d7c79ad8508dbbfb7aeed49b85d4
Jonah <jjmgruber@gmail.com>
2013-10-28 23:35:47 -0400

    Will now show dialog instead of crashing when input fields are blank

--- android/src/com/aj3/kiss/AddItemActivity.java
+++ android/src/com/aj3/kiss/AddItemActivity.java
@@ -1,6 +1,8 @@
 package com.aj3.kiss;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -61,17 +63,41 @@ public class AddItemActivity extends Activity {
 	}
 	
 	public void addItem() {
-		Intent intent = getIntent();
-		String callSource = intent.getStringExtra(this.ACTIVITY_CALLER);
-		if(callSource.equals(InventoryActivity.NAME)){
-			Toast.makeText(getApplicationContext(), "Adding Item to " + callSource, Toast.LENGTH_LONG).show();
-			this.addItemToInventory();
-		}else if(callSource.equals(GroceryActivity.NAME)) {
-			Toast.makeText(getApplicationContext(), "Adding Item to " + callSource, Toast.LENGTH_LONG).show();
-			this.addItemToGrocery();
+		if(checkIfValid()) {
+//			Intent intent = getIntent();
+//			String callSource = intent.getStringExtra(this.ACTIVITY_CALLER);
+//			if(callSource.equals(InventoryActivity.NAME)){
+//				Toast.makeText(getApplicationContext(), "Adding Item to " + callSource, Toast.LENGTH_LONG).show();
+//				this.addItemToInventory();
+//			}else if(callSource.equals(GroceryActivity.NAME)) {
+//				Toast.makeText(getApplicationContext(), "Adding Item to " + callSource, Toast.LENGTH_LONG).show();
+//				this.addItemToGrocery();
+//			}
+//			this.finish();
+		} else {
+			
 		}
-		this.finish();
 		
+	}
+	
+	public boolean checkIfValid(){
+		if(mNameView.getText().toString().trim().isEmpty() || 
+				mCategoryView.getText().toString().trim().isEmpty() || 
+				mQuantityView.getText().toString().trim().isEmpty()) {
+			new AlertDialog.Builder(this)
+	        .setTitle("Blank Fields")
+	        .setMessage("You cannot leave fields blank")
+	        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+	            public void onClick(DialogInterface dialog, int which) { 
+	            }
+	         })
+	         .show();
+			Toast.makeText(getApplicationContext(), "False", Toast.LENGTH_LONG).show();
+			return false;
+		} else {
+			Toast.makeText(getApplicationContext(), "True" + mNameView.toString(), Toast.LENGTH_LONG).show();
+			return true;
+		}
 	}
 
 	public void addItemToInventory() {
