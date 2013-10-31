71f2de6da48c5b290c01ace993a53e661186c83d
Jonah <jjmgruber@gmail.com>
2013-10-31 01:42:17 -0400

    returning the category and unit class

---
+++ android/src/com/aj3/kiss/Unit.java
@@ -0,0 +1,29 @@
+package com.aj3.kiss;
+
+public class Unit {
+	private int id;
+	private String name;
+	
+	public Unit(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	public Unit() {}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
