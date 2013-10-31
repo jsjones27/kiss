2af25a410265bc8ba3c44141e9e38742f0b0f24f
AishwaryaV <venkaa4@rpi.edu>
2013-10-30 22:49:49 -0400

    Added IntentIntegrator and IntentResult classes

---
+++ android/src/com/example/kiss/ListItem.java
@@ -0,0 +1,29 @@
+package com.example.kiss;
+
+public class ListItem {
+	private Item item;
+	private double quantity;
+	
+	public ListItem(Item item, double quantity) {
+		this.item = item;
+		this.quantity = quantity;
+	}
+	
+	public ListItem() {}
+
+	public Item getItem() {
+		return item;
+	}
+
+	public void setItem(Item item) {
+		this.item = item;
+	}
+
+	public double getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(double quantity) {
+		this.quantity = quantity;
+	}
+}
---
+++ android/src/com/example/kiss/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.kiss;
+
+import java.util.Vector;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		/* testing code */
+		/*DatabaseHelper db = new DatabaseHelper(this);
+		db.resetDatabase();
+		db.addItem(new Item("C", "A"));
+		db.addItem(new Item("B", "B"));
+		db.addInventoryItem(new ListItem(db.getItem(1), 1));
+		db.addInventoryItem(new ListItem(db.getItem(2), 1));
+		db.deleteInventoryItem(new ListItem(db.getItem(1), 1));
+		db.updateInventoryItem(new ListItem(db.getItem(2), 2));
+		Vector<ListItem> inventory = db.getInventory();
+		for (ListItem listItem : inventory) {
+			System.out.println(listItem.getItem().getName());
+			System.out.println(listItem.getQuantity());
+		}
+		db.close();*/
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	public void jumpToInventory(View view){
+		Intent intent = new Intent(this, InventoryActivity.class);
+		startActivity(intent);
+	}
+	
+	public void jumpToGrocery(View view){
+		Intent intent = new Intent(this, GroceryActivity.class);
+		startActivity(intent);
+	}
+
+}
