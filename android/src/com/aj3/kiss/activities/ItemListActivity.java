ae26af9dc78cdd630c4fa136ee9f9b18947c730d
Jonah <jjmgruber@gmail.com>
2013-11-20 18:02:40 -0500

    Added alertDialog to say the quantity of the iteam

--- android/src/com/aj3/kiss/activities/ItemListActivity.java
+++ android/src/com/aj3/kiss/activities/ItemListActivity.java
@@ -73,12 +73,31 @@ public abstract class ItemListActivity extends Activity {
 
 			@Override
 			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-				showMoveDialog(listItems.get(arg2));
+				showInfoDialog(listItems.get(arg2));
 			}
 		 }); 
 		
 	}
 
+	protected void showInfoDialog(final ListItem listItem) {
+		// Create an instance of the dialog fragment and show it
+		new AlertDialog.Builder(this)
+		.setTitle(listItem.getItem().getName())
+		.setMessage("You have " + listItem.getQuantity() + " " + listItem.getItem().getUnit())
+		.setPositiveButton("Move", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) { 
+				showMoveDialog(listItem);
+			}
+		})
+		.setNegativeButton("Ok", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) { 
+				// do nothing
+			}
+		})
+		.show();
+		
+	}
+	
     protected abstract void showMoveDialog(ListItem listItem) ;
 
 	public void showDeleteDialog(final ListItem li) {
